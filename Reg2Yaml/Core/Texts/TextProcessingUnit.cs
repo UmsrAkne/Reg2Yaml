faec1da85ce8c8d7a568bb2750bcf39d661ac7ce
UmsrAkne <sibata486@gmail.com>
2026-06-18 22:44:05 +0900

    feat: Implement UI for text processing configuration and bind to ViewModel

--- Reg2Yaml/Core/Texts/TextProcessingUnit.cs
+++ Reg2Yaml/Core/Texts/TextProcessingUnit.cs
@@ -5,8 +5,13 @@ namespace Reg2Yaml.Core.Texts
     public class TextProcessingUnit : BindableBase
     {
         private string regexPattern = string.Empty;
+        private TextProcessingType processingType;
 
-        private TextProcessingType ProcessingType { get; set; }
+        public TextProcessingType ProcessingType
+        {
+            get => processingType;
+            set => SetProperty(ref processingType, value);
+        }
 
         public string RegexPattern { get => regexPattern; set => SetProperty(ref regexPattern, value); }
     }
--- Reg2Yaml/ViewModels/MainWindowViewModel.cs
+++ Reg2Yaml/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using Prism.Mvvm;
+using Reg2Yaml.Core.Texts;
 using Reg2Yaml.Utils;
 
 namespace Reg2Yaml.ViewModels;
@@ -17,6 +19,8 @@ public class MainWindowViewModel : BindableBase
     #endif
 
     private readonly AppVersionInfo appVersionInfo = new();
+    private TextProcessorContainer selectedContainer;
+    private TextProcessor selectedProcessor;
 
     public MainWindowViewModel()
     {
@@ -25,8 +29,80 @@ public class MainWindowViewModel : BindableBase
 
     public string Title => appVersionInfo.Title;
 
+    public ObservableCollection<TextProcessorContainer> TextProcessorContainers { get; set; } = new();
+
+    public TextProcessorContainer SelectedContainer
+    {
+        get => selectedContainer;
+        set => SetProperty(ref selectedContainer, value);
+    }
+
+    public TextProcessor SelectedProcessor
+    {
+        get => selectedProcessor;
+        set => SetProperty(ref selectedProcessor, value);
+    }
+
     [Conditional("DEBUG")]
     private void SetupDummyData()
     {
+        // 1つ目のコンテナ：ユーザー情報に関するテキスト処理
+        var userProfileContainer = new TextProcessorContainer
+        {
+            DisplayName = "ユーザー情報パース設定",
+            TextProcessors = new ObservableCollection<TextProcessor>
+            {
+                new()
+                {
+                    Caption = "UserNameExtractor",
+                    Units = new ObservableCollection<TextProcessingUnit>
+                    {
+                        new()
+                        {
+                            ProcessingType = TextProcessingType.Extract, RegexPattern = @"ID:\d+\s+Name:(?<name>\w+)",
+                        },
+                        new() { ProcessingType = TextProcessingType.Replace, RegexPattern = @"\s+", },
+                    },
+                },
+                new()
+                {
+                    Caption = "EmailMasker",
+                    Units = new ObservableCollection<TextProcessingUnit>
+                    {
+                        new()
+                        {
+                            ProcessingType = TextProcessingType.Replace, RegexPattern = @"(?<=.)[^@](?=[^@]*?[^@].*?@)",
+                        },
+                    },
+                },
+            },
+        };
+
+        // 2つ目のコンテナ：ログ解析に関するテキスト処理
+        var logAnalysisContainer = new TextProcessorContainer
+        {
+            DisplayName = "サーバーログ解析設定",
+            TextProcessors = new ObservableCollection<TextProcessor>
+            {
+                new()
+                {
+                    Caption = "ErrorLogFilter",
+                    Units = new ObservableCollection<TextProcessingUnit>
+                    {
+                        new()
+                        {
+                            ProcessingType = TextProcessingType.Extract, RegexPattern = @"\[ERROR\]\s+(?<message>.*)",
+                        },
+                    },
+                },
+            },
+        };
+
+        // コレクションに追加
+        TextProcessorContainers.Add(userProfileContainer);
+        TextProcessorContainers.Add(logAnalysisContainer);
+
+        // 初期選択状態として1つ目のコンテナをセット
+        SelectedContainer = userProfileContainer;
     }
 }
